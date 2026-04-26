92cb4a9b3be8d05f40238968d403084f4628f74a
Цыбина Евгения <tsybina.evgeniya@yandex.ru>
2026-04-26 14:44:44 +0300

    create welcome message

--- SimpleTGBot/TelegramBot.cs
+++ SimpleTGBot/TelegramBot.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using static System.Console;
+using System.Text.RegularExpressions;
 
 namespace SimpleTGBot;
 using Telegram.Bot;
@@ -9,9 +11,9 @@ using Telegram.Bot.Types.Enums;
 
 public class TelegramBot
 {
-    // Токен TG-бота. Можно получить у @BotFather
-    private const string BotToken = "ВАШ_ТОКЕН_ИДЕНТИФИКАЦИИ_БОТА";
-    
+    // Токен TG-бота.
+    private const string BotToken = "8622644979:AAHzcHAXZ2SfMjEWsOnWp6KGNHAx8ozjFus";
+
     /// <summary>
     /// Инициализирует и обеспечивает работу бота до нажатия клавиши Esc
     /// </summary>
@@ -20,18 +22,18 @@ public class TelegramBot
         // Если вам нужно хранить какие-то данные во время работы бота (массив информации, логи бота,
         // историю сообщений для каждого пользователя), то это всё надо инициализировать в этом методе.
         // TODO: Инициализация необходимых полей
-        
+
         // Инициализируем наш клиент, передавая ему токен.
         var botClient = new TelegramBotClient(BotToken);
-        
+
         // Служебные вещи для организации правильной работы с потоками
         using CancellationTokenSource cts = new CancellationTokenSource();
-        
+
         // Разрешённые события, которые будет получать и обрабатывать наш бот.
         // Будем получать только сообщения. При желании можно поработать с другими событиями.
         ReceiverOptions receiverOptions = new ReceiverOptions()
         {
-            AllowedUpdates = new [] { UpdateType.Message }
+            AllowedUpdates = new[] { UpdateType.Message }
         };
 
         // Привязываем все обработчики и начинаем принимать сообщения для бота
@@ -44,15 +46,38 @@ public class TelegramBot
 
         // Проверяем что токен верный и получаем информацию о боте
         var me = await botClient.GetMeAsync(cancellationToken: cts.Token);
-        Console.WriteLine($"Бот @{me.Username} запущен.\nДля остановки нажмите клавишу Esc...");
-        
+        WriteLine($"Бот @{me.Username} запущен.\nДля остановки нажмите клавишу Esc...");
+
         // Ждём, пока будет нажата клавиша Esc, тогда завершаем работу бота
-        while (Console.ReadKey().Key != ConsoleKey.Escape){}
+        while (ReadKey().Key != ConsoleKey.Escape) { }
 
         // Отправляем запрос для остановки работы клиента.
         cts.Cancel();
     }
-    
+
+    /// <summary>
+    /// реакция на команду /start. 
+    /// </summary>
+    /// <param name="botClient"></param>
+    /// <param name="chatId"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    private async Task SendWelcomeMessage(ITelegramBotClient botClient, long chatId, CancellationToken ct)
+    {
+        string fileName = $"{chatId}.txt";
+
+        if (System.IO.File.Exists(fileName))
+        {
+            string farmName = await System.IO.File.ReadAllTextAsync(fileName);
+            await botClient.SendTextMessageAsync(chatId, $"С возвращением на ферму <b>{farmName}</b>!", parseMode: ParseMode.Html, cancellationToken: ct);
+        }
+        else
+        {
+            string welcomeText = "Добро пожаловать!\n\nЧтобы начать, дай своей ферме имя.\n\nНапиши сообщение в формате:\n<b>Моя ферма: Название</b>";
+            await botClient.SendTextMessageAsync(chatId, welcomeText, parseMode: ParseMode.Html, cancellationToken: ct);
+        }
+    }
+
     /// <summary>
     /// Обработчик события получения сообщения.
     /// </summary>
@@ -61,36 +86,50 @@ public class TelegramBot
     /// <param name="cancellationToken">Служебный токен для работы с многопоточностью</param>
     async Task OnMessageReceived(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        // Работаем только с сообщениями. Остальные события игнорируем
         var message = update.Message;
-        if (message is null)
-        {
-            return;
-        }
-        // Будем обрабатывать только текстовые сообщения.
-        // При желании можно обрабатывать стикеры, фото, голосовые и т. д.
-        //
-        // Обратите внимание на использованную конструкцию. Она эквивалентна проверке на null, приведённой выше.
-        // Подробнее об этом синтаксисе: https://medium.com/@mattkenefick/snippets-in-c-more-ways-to-check-for-null-4eb735594c09
-        if (message.Text is not { } messageText)
-        {
-            return;
-        }
+        if (message is null) return;
 
-        // Получаем ID чата, в которое пришло сообщение. Полезно, чтобы отличать пользователей друг от друга.
         var chatId = message.Chat.Id;
-        
-        // Печатаем на консоль факт получения сообщения
-        Console.WriteLine($"Получено сообщение в чате {chatId}: '{messageText}'");
+        string fileName = $"{chatId}.txt";
 
-        // TODO: Обработка пришедших сообщений
-        
-        // Отправляем обратно то же сообщение, что и получили
-        Message sentMessage = await botClient.SendTextMessageAsync(
-            chatId: chatId,
-            text: "Ты написал:\n" + messageText,
-            cancellationToken: cancellationToken);
+        string messageText = message.Text;
+        WriteLine($"Получено сообщение в чате {chatId}: '{messageText}'");
+
+        string command = messageText.ToLower().Trim();
+        switch (command)
+        {
+            case "/start":
+                await SendWelcomeMessage(botClient, chatId, cancellationToken);
+                break;
+
+            default:
+                if (System.IO.File.Exists(fileName))
+                {
+                    string savedFarmName = await System.IO.File.ReadAllTextAsync(fileName);
+
+                    if (command == "привет")
+                        await botClient.SendTextMessageAsync(chatId, "Привет!");
+
+                    else
+                        await botClient.SendTextMessageAsync(chatId, "Команда принята! Скоро здесь появятся грядки.");
+                }
+                else
+                {
+                    var farmMatch = Regex.Match(messageText, @"^Моя ферма:\s*(.+)$", RegexOptions.IgnoreCase);
+
+                    if (farmMatch.Success)
+                    {
+                        string farmName = farmMatch.Groups[1].Value;
+                        await System.IO.File.WriteAllTextAsync(fileName, farmName);
+                        await botClient.SendTextMessageAsync(chatId, $"Чудесно! Теперь твоя ферма официально называется <b>{farmName}</b>!", parseMode: ParseMode.Html);
+                    }
+                    else
+                        await botClient.SendTextMessageAsync(chatId, "Прости, я не понимаю. Сначала дай ферме имя в формате:\n<b>Моя ферма: Название</b>", parseMode: ParseMode.Html);
+                }
+                break;
+        }
     }
+
 
     /// <summary>
     /// Обработчик исключений, возникших при работе бота
@@ -106,12 +145,12 @@ public class TelegramBot
         {
             ApiRequestException apiRequestException
                 => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-            
+
             _ => exception.ToString()
         };
 
-        Console.WriteLine(errorMessage);
-        
+        WriteLine(errorMessage);
+
         // Завершаем работу
         return Task.CompletedTask;
     }
